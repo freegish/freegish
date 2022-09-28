3b2dab264d9d11992b50b24b40abb2fcd9bf87ed
Wouter Wijsman <wwijsman@live.nl>
2022-09-28 22:09:11 +0200

    Link to SDL2

--- src/sdl/sdl.h
+++ src/sdl/sdl.h
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifdef WINDOWS
 #include <SDL.h>
 #else
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
 #endif
 
 #endif /* GISH_SDL_VIDEO_H */
