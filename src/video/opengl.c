8d76068338d29ff78aae2f8c0542588f3935c5eb
Bob van der Linden <bobvanderlinden@gmail.com>
2022-10-19 14:44:58 +0200

    Refer to SDL2 header file

--- src/video/opengl.c
+++ src/video/opengl.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../video/opengl.h"
 
 #ifdef WINDOWS
-  #include <SDL.h>
+  #include <SDL2/SDL.h>
 #endif
 
 #include <string.h>
